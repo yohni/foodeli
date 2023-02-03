01ba836b1f6c2c2cdc336252051ad503e3a211b4
yohni <yohni.123@gmail.com>
2023-02-03 11:22:54 +0700

    fix: 🐛 adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,11 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			edge: false,
-			external: [],
-			split: false
-		})
+		adapter: adapter()
 	}
 };
 
