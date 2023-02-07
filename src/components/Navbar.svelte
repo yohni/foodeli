7bb78e1f53e23f8e63261c04e891fcbf1fc4dfaf
yohni <yohni.123@gmail.com>
2023-02-07 22:18:38 +0700

    feat: 🎸 responsive hero

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,15 +1,65 @@
 <script lang="ts">
 	import Logo from './Logo.svelte';
 	import Icon from '@iconify/svelte';
+	import clsx from 'clsx';
+
+	const MENU = ['Why Foodeli?', 'Services', 'Menu', 'Contact'];
 </script>
 
-<nav class="container flex justify-between">
+<nav class="container flex justify-between items-center">
 	<div class="flex gap-4 items-center">
-		<Logo className="w-6 h-6" />
+		<Logo className="w-6 h-6 md:w-11 md:h-11" />
 		<h4 class="font-semibold text-lg md:text-2xl">Foodeli</h4>
 	</div>
 
-	<button class="text-xl">
+	<div>
+		<ul class="flex gap-7">
+			{#each MENU as m, i}
+				<li
+					class={clsx('text-sm text-base-black-2 relative hover:text-red-primary cursor-pointer', {
+						'!text-red-primary': i === 0
+					})}
+				>
+					<div>
+						{m}
+					</div>
+					{#if i === 0}
+						<div
+							class={clsx(
+								'bg-red-primary w-[6px] h-[6px] rounded-full',
+								'transform -translate-x-1/2 absolute left-1/2 top-full mt-2'
+							)}
+						/>
+					{/if}
+				</li>
+			{/each}
+		</ul>
+	</div>
+
+	<button class="text-xl xl:hidden">
 		<Icon icon="charm:menu-hamburger" />
 	</button>
+
+	<div class="flex items-center gap-x-6">
+		<button class="btn-hover">
+			<Icon icon="akar-icons:search" class="text-2xl" />
+		</button>
+		<button class="relative h-min btn-hover">
+			<Icon icon="akar-icons:shopping-bag" class="text-2xl" />
+			<div
+				class="text-[10px] px-1 font-bold rounded-full bg-red-primary text-white absolute -top-[6px] -right-[6px]"
+			>
+				5
+			</div>
+		</button>
+		<button
+			class={clsx(
+				'flex items-center gap-2',
+				'bg-red-primary text-white rounded-full px-6 py-3',
+				'btn-hover'
+			)}
+		>
+			<Icon icon="carbon:login" /> <span>Login</span>
+		</button>
+	</div>
 </nav>
--- src/views/Hero.svelte
+++ src/views/Hero.svelte
@@ -1,50 +1,70 @@
 <script lang="ts">
+	import { fly } from 'svelte/transition';
 	import clsx from 'clsx';
 	import Icon from '@iconify/svelte';
+	import { inview } from 'svelte-inview';
+
+	let isInView: boolean;
 </script>
 
-<div>
-	<div class="container flex flex-col gap-6">
-		<div class="py-8">
-			<button
-				type="button"
-				class={clsx(
-					'py-3 px-5 mb-12 flex gap-4 items-center text-base',
-					'bg-[#FEE9DE] text-red-primary rounded-full'
-				)}
-			>
-				<span>More than Faster</span>
-				<img src="Cherry.png" alt="Cherry" />
-			</button>
-
-			<h1 class="text-6xl font-rubik text-base-black-1 mb-6">
-				Claim Best Offer on Fast <span class="text-7xl text-red-primary font-lobster2 italic"
-					>Food</span
-				>
-				& <span class="text-7xl text-red-primary font-lobster2 italic">Restaurants</span>
-			</h1>
-
-			<p class="text-base-black-2 text-lg">
-				Our job is to filling your tummy with delicious food and with fast and free delivery
-			</p>
-
-			<div class="mt-6 flex flex-col gap-6">
+<header
+	use:inview={{ unobserveOnEnter: true }}
+	on:change={({ detail }) => {
+		isInView = detail.inView;
+	}}
+>
+	{#if isInView}
+		<div class="container flex flex-col xl:flex-row gap-6">
+			<div class="py-8">
 				<button
+					in:fly={{ y: 200, duration: 250 }}
 					type="button"
-					class="bg-red-primary py-6 px-10 w-full text-white rounded-full text-lg"
+					class={clsx(
+						'py-3 px-5 mb-12 flex gap-4 items-center text-base',
+						'bg-[#FEE9DE] text-red-primary rounded-full'
+					)}
 				>
-					Get Started
+					<span>More than Faster</span>
+					<img src="Cherry.png" alt="Cherry" />
 				</button>
-				<button type="button" class="flex gap-4 items-center justify-center group">
-					<div
-						class="bg-[#F2C94C] rounded-full w-12 h-12 flex justify-center items-center text-2xl group-hover:animate-pulse"
+
+				<h1
+					in:fly={{ y: 200, duration: 250, delay: 100 }}
+					class="text-6xl font-rubik text-base-black-1 mb-6"
+				>
+					Claim Best Offer on Fast <span class="text-7xl text-red-primary font-lobster2 italic"
+						>Food</span
 					>
-						<Icon icon="material-symbols:play-arrow-rounded" />
-					</div>
-					<span class="text-lg text-base-black-2"> Watch Video </span>
-				</button>
+					& <span class="text-7xl text-red-primary font-lobster2 italic">Restaurants</span>
+				</h1>
+
+				<p in:fly={{ y: 200, duration: 250, delay: 200 }} class="text-base-black-2 text-lg">
+					Our job is to filling your tummy with delicious food and with fast and free delivery
+				</p>
+
+				<div in:fly={{ y: 200, duration: 250, delay: 300 }} class="mt-6 grid xl:grid-cols-2 gap-6">
+					<button
+						type="button"
+						class={clsx(
+							'bg-red-primary py-6 px-10 w-full text-white rounded-full text-lg',
+							'btn-hover'
+						)}
+					>
+						Get Started
+					</button>
+					<button type="button" class="flex gap-4 items-center justify-center group">
+						<div
+							class="bg-[#F2C94C] rounded-full w-12 h-12 flex justify-center items-center text-2xl group-hover:animate-pulse"
+						>
+							<Icon icon="material-symbols:play-arrow-rounded" />
+						</div>
+						<span class="text-lg text-base-black-2"> Watch Video </span>
+					</button>
+				</div>
+			</div>
+			<div>
+				<img loading="lazy" src="Food1.webp" alt="Food illustrations" />
 			</div>
 		</div>
-		<img loading="lazy" src="Food1.webp" alt="Food illustrations" />
-	</div>
-</div>
+	{/if}
+</header>
